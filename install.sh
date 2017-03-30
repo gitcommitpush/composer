eb4311b4dc6547f9c41c190ab202a98d55e8a376
Jordan Brown <gitcommitpush@users.noreply.github.com>
2017-03-30 17:55:34 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt-get update && apt-get upgrade -y
+apt-get install git python3 python3-pip -y
+
+git clone https://github.com/gitcommitpush/composer.git
+cd composer
+
+pip3 install -r requirements.txt
+
+mkdir data
+mkdir data/apps
+mkdir data/log
