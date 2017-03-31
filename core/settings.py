cf11e6d449d98cb20cc8a2007ffce9efada5cb9d
Jordan Brown <gitcommitpush@users.noreply.github.com>
2017-03-31 13:54:56 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -5,4 +5,4 @@ DATA_DIR = os.path.join(BASE_DIR, 'data')
 APP_DIR = os.path.join(DATA_DIR, 'apps')
 
 BASE_DOMAIN = 'example.com'
-NGINX_CONFIG_LOCATION = os.path.join(BASE_DIR, 'data')  # Temporary
+NGINX_CONFIG_LOCATION = os.path.join('/', 'etc', 'nginx', 'conf.d')
