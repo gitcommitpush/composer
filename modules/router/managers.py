ca1e3120053e2d824fa54dc974004cdbe03c9447
Jordan Brown <jordan@dabapps.com>
2017-04-04 22:49:10 +0100

    Add extra to logger warn

--- modules/router/managers.py
+++ modules/router/managers.py
@@ -4,6 +4,7 @@ from modules.apps.managers import AppManager
 import os
 import subprocess
 import socket
+import time
 
 
 class RouterManager(BaseManager):
@@ -26,6 +27,15 @@ class RouterManager(BaseManager):
         sock.close()
         return available_port
 
+    def run_pre_check(self, port):
+        self.logger.info('Running pre-checks...')
+        try:
+            subprocess.check_output(['nc', '-z', '127.0.0.1', port])
+        except subprocess.CalledProcessError:
+            self.logger.fail('Container is not listening on port {}'.format(port))
+
+        self.logger.info('Pre-checks successful.')
+
     def define_route(self, port):
         self.logger.info('Routing hostname to container...')
         content = self.get_config_template().format(hostname=self.get_hostname(), port=port)
@@ -38,12 +48,15 @@ class RouterManager(BaseManager):
             self.logger.fail('Routing failed: could not write proxy config file')
 
         try:
-            subprocess.check_output(['service', 'nginx', 'restart'], stderr=subprocess.STDOUT)
+            process = subprocess.check_output(['service', 'nginx', 'restart'], stderr=subprocess.STDOUT)
         except FileNotFoundError:
             self.logger.fail('Routing failed: command "service" not found. Possibly an unsupported operating system?')
         except subprocess.CalledProcessError as error:
             self.logger.warn('Routing failed: could not restart proxy service', extra=error.output)
 
         self.logger.info('Routing successful.')
+
+        self.run_pre_check(port)
+
         self.logger.info('Map [ {} => 127.0.0.1:{} ]'.format(self.get_hostname(), port))
         self.logger.info('You can also access this application via port {}'.format(port))
